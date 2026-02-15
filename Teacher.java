6b45e9dc622ff6d7b5a45bbec0c941bb78b52c65
Dhivya Venkatesan <dhivyavenkatesan96@gmail.com>
2026-02-15 10:46:07 +0530

    Add files via upload

---
+++ Teacher.java
@@ -0,0 +1,16 @@
+public class Teacher {
+    private int id;
+    private String name;
+    private String subject;
+
+    public Teacher(int id, String name, String subject) {
+        this.id = id;
+        this.name = name;
+        this.subject = subject;
+    }
+
+    @Override
+    public String toString() {
+        return "ID: " + id + ", Name: " + name + ", Subject: " + subject;
+    }
+}
